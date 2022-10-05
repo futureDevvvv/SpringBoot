2f2cf48edfaa8780e66421198d3d4b82720d20f1
Barcode7 <nox8935@gmail.com>
2022-10-05 15:54:01 +0900

    신승훈 10/5 버그수정

--- issho/src/main/java/net/softsociety/issho/notice/dao/NoticeDAO.java
+++ issho/src/main/java/net/softsociety/issho/notice/dao/NoticeDAO.java
@@ -51,6 +51,7 @@ public interface NoticeDAO {
 	
 	// 댓글 삭제
 	public int deleteComment(Comment comment);
-
+	
+	//* 신승훈 메인창 목록 (최신 공지사항)
 	public List<Notice> selectMainNotice(String prj_domain);
 }
